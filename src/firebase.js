8e0ff004983c061e74ad68a6c3452c0191fa2695
C1oudys <hymin310@gmail.com>
2024-02-07 20:20:33 +0900

    add: firebase

---
+++ src/firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDNLgL207w9aAoVlKqzQK_ieeROhfHtusE",
+  authDomain: "groove-project-aef59.firebaseapp.com",
+  projectId: "groove-project-aef59",
+  storageBucket: "groove-project-aef59.appspot.com",
+  messagingSenderId: "1079215030068",
+  appId: "1:1079215030068:web:fa25b7d3b1604d0779af61"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);

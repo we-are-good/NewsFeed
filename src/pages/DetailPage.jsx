6d5517336d0e8c01d4600f3a66cc9a8abc829a30
rlaeodus <gwg03185@gmail.com>
2024-02-12 19:56:38 +0900

    Fix : DetailPage.jsx - 삭제기능

--- src/pages/DetailPage.jsx
+++ src/pages/DetailPage.jsx
@@ -1,13 +1,15 @@
 import { deleteDoc, doc } from "firebase/firestore";
 import React from "react";
-import { useLocation, useParams } from "react-router-dom";
+import { useLocation, useNavigate, useParams } from "react-router-dom";
 import { db } from "../firebase";
 
-function DetailPage({ setGrooveTop, GrooveTop }) {
+function DetailPage() {
+  const navigate = useNavigate();
   const params = useParams();
   const location = useLocation();
-  const detailGroove = location.state.filter((item) => item.id === params.id);
-  console.log(...detailGroove);
+  const detailGroove = location.state.find((item) => item.id === params.id);
+  console.log("location", location);
+  console.log(detailGroove);
   // const updateTodo = async (event) => {
   //   const todoRef = doc(db, "todos", todo.id);
   //   await updateDoc(todoRef, { ...todo, isDone: !todo.isDone });
@@ -22,23 +24,24 @@ function DetailPage({ setGrooveTop, GrooveTop }) {
   //     });
   //   });
   // };
-
-  const deleteGroove = async (event) => {
-    const GrooveTopRef = doc(db, "GrooveTop", GrooveTop.id);
-    console.log(GrooveTopRef);
-    await deleteDoc(GrooveTopRef);
-
-    setGrooveTop((prev) => {
-      console.log(prev);
-      return prev.filter((element) => element.id !== GrooveTop.id);
-    });
+  const deleteGroove = async () => {
+    try {
+      const askDelete = window.confirm("정말 삭제하시겠습니까?");
+      if (!askDelete) return;
+      const GrooveTopRef = doc(db, "GrooveTop", detailGroove.id);
+      console.log("GrooveTopRef:", GrooveTopRef); // 추가된 로그
+      await deleteDoc(GrooveTopRef, alert("삭제되었습니다"));
+      navigate("/");
+    } catch (error) {
+      console.error("Error deleting groove:", error);
+    }
   };
 
   return (
     <>
       <div>DetailPage</div>
-      <>제목: {detailGroove[0].title}</>
-      <>내용: {detailGroove[0].body}</>
+      <>제목: {detailGroove.title}</>
+      <>내용: {detailGroove.body}</>
       <button onClick={deleteGroove}>삭제하기</button>
     </>
   );

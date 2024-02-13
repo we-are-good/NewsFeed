3f87f13955363dcb4015a8cadcdf1132a21b5bde
illu_y <ii_2002@naver.com>
2024-02-14 03:28:02 +0900

    기능개선 - 좋아요버튼 상태 유지

--- src/components/Groove/GrooveTotalFeed/GrooveLikeBtn.jsx
+++ src/components/Groove/GrooveTotalFeed/GrooveLikeBtn.jsx
@@ -1,9 +1,17 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import { updateDoc, doc } from "firebase/firestore";
 import { db } from "../../../firebase";
 
 const GrooveLikeBtn = ({ initialIsLiked, onLikeClick, likeCount, grooveId }) => {
   const [isLiked, setIsLiked] = useState(initialIsLiked);
+
+  // localStorage에서 좋아요 상태를 불러오기
+  useEffect(() => {
+    const storedIsLiked = localStorage.getItem(`like-${grooveId}`);
+    if (storedIsLiked !== null) {
+      setIsLiked(JSON.parse(storedIsLiked));
+    }
+  }, [grooveId]);
 
   const toggleLike = async () => {
     // Firestore 업데이트
@@ -15,6 +23,9 @@ const GrooveLikeBtn = ({ initialIsLiked, onLikeClick, likeCount, grooveId }) => 
 
     // 부모 컴포넌트에서 좋아요 상태를 업데이트하는 함수 호출
     onLikeClick();
+
+    // localStorage에 좋아요 상태 저장
+    localStorage.setItem(`like-${grooveId}`, JSON.stringify(!isLiked));
 
     // 로컬 상태 업데이트
     setIsLiked(!isLiked);

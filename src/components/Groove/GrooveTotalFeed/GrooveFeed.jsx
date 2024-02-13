1cbb9e3c8678346422c41f9f444ef1125914f032
illu_y <ii_2002@naver.com>
2024-02-14 03:12:02 +0900

    오류 해결 - null값 문제

--- src/components/Groove/GrooveTotalFeed/GrooveFeed.jsx
+++ src/components/Groove/GrooveTotalFeed/GrooveFeed.jsx
@@ -27,7 +27,7 @@ const GrooveFeed = ({ GrooveTop, setGrooveTop }) => {
                 {/* 좋아요 버튼 및 갯수 렌더링 */}
                 <UserLikeBox>
                   <p>
-                    <i className="fa-solid fa-heart" /> {Object.keys(item.likes).length}개
+                    <i className="fa-solid fa-heart" /> {Object.keys(item.likes || {}).length}개
                   </p>
                 </UserLikeBox>
               </ContentWrapBox>

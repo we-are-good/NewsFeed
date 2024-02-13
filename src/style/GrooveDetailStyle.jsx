84c34957eb034f79860707ea97c9e64ee08a1d6e
rlaeodus <gwg03185@gmail.com>
2024-02-14 05:46:01 +0900

    dev pull전 커밋해놓기

--- src/style/GrooveDetailStyle.jsx
+++ src/style/GrooveDetailStyle.jsx
@@ -1,5 +1,29 @@
 import styled from "styled-components";
 
+export const EditingWrap = styled.div`
+  display: flex;
+  background-color: blue;
+  flex-direction: column;
+
+  align-items: center;
+`;
 export const EditingTitle = styled.h1`
   font-size: 1rem;
+`;
+export const EditButtonWrap = styled.div`
+  display: flex;
+`;
+
+export const Wrap = styled.div`
+  background-color: red;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column-reverse;
+`;
+export const Title = styled.div`
+  display: flex;
+`;
+export const Body = styled.div`
+  display: flex;
 `;

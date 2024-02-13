19a2f37a04e1cd74f9b32d8997f59c957dae1a44
C1oudys <hymin310@gmail.com>
2024-02-13 20:23:51 +0900

    fix: mypage

--- src/pages/Mypage.jsx
+++ src/pages/Mypage.jsx
@@ -1,17 +1,9 @@
-<<<<<<< HEAD
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import { auth, db } from "../firebase";
 import GrooveHeader from "../components/Groove/GrooveHeader";
 import GrooveFooter from "../components/Groove/GrooveFooter";
 import styled from 'styled-components';
-=======
-import React, { useEffect, useState } from "react";
-import { auth, db } from "../firebase";
-import GrooveHeader from "../components/Groove/GrooveHeader";
-import GrooveFooter from "../components/Groove/GrooveFooter";
-import styled from "styled-components";
->>>>>>> 8dd347eaf5df7104d628074eda5df42f1676bfbb
 
 function MyPage() {
   const [currentUser, setCurrentUser] = useState(null);
@@ -36,7 +28,6 @@ function MyPage() {
       unsubscribe();
     };
   }, []);
-<<<<<<< HEAD
   // 사용자 정보를 가져오는 함수
   const fetchData = async (email) => {
     try {
@@ -48,16 +39,6 @@ function MyPage() {
         const nickname = userData.nickname;
         console.log("User's nickname:", nickname); // 닉네임을 콘솔에 출력
         setUserNickname(nickname); // 받아온 닉네임을 userNickname에 설정
-=======
-
-  useEffect(() => {
-    // 현재 사용자가 작성한 글 목록 가져오기
-    const fetchUserPosts = async () => {
-      if (currentUser) {
-        const querySnapshot = await db.collection("posts").where("userId", "==", currentUser.uid).get();
-        const userPostsData = querySnapshot.docs.map((doc) => ({ id: doc.id, ...doc.data() }));
-        setUserPosts(userPostsData);
->>>>>>> 8dd347eaf5df7104d628074eda5df42f1676bfbb
       }
     } catch (error) {
       console.error('Error fetching user info:', error);
@@ -79,7 +60,6 @@ function MyPage() {
           <StDiv>
             <div>
               <p>사용자 정보:</p>
-<<<<<<< HEAD
               <p>이름: {userNickname}</p> {/* 닉네임을 이름으로 출력 */}
               <p>이메일: {userEmail}</p> {/* 이메일 표시 */}
               <p>작성한 글 목록:</p>
@@ -90,20 +70,6 @@ function MyPage() {
             <p>로그인이 필요합니다.</p>
             <Link to="/login">로그인하러 가기</Link>
           </StyledMessage>
-=======
-              <p>이름: {currentUser.displayName}</p>
-              <p>이메일: {currentUser.email}</p>
-              <p>작성한 글 목록:</p>
-            </div>
-            <StList>
-              {userPosts.map((post) => (
-                <li key={post.id}>{post.title}</li>
-              ))}
-            </StList>
-          </StDiv>
-        ) : (
-          <p>로그인이 필요합니다.</p>
->>>>>>> 8dd347eaf5df7104d628074eda5df42f1676bfbb
         )}
       </StDiv>
       <GrooveFooter />

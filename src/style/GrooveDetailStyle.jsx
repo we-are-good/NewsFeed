5a056fcaa4a57e72d35abd5ae1190d462cc08795
illu_y <ii_2002@naver.com>
2024-02-15 02:01:32 +0900

    Likebtn motion make

--- src/style/GrooveDetailStyle.jsx
+++ src/style/GrooveDetailStyle.jsx
@@ -1,4 +1,5 @@
-import styled from "styled-components";
+// import styled from "styled-components";
+import styled, { keyframes } from "styled-components";
 
 export const EditingWrap = styled.div`
   display: flex;
@@ -102,6 +103,39 @@ export const Body = styled.div`
   }
 `;
 
+const pangpang = keyframes`
+0% {
+    background-position: 5% 90%, 10% 90%, 10% 90%, 15% 90%, 25% 90%, 25% 90%,
+      40% 90%, 55% 90%, 70% 90%;
+  }
+
+  50% {
+    background-position: 0% 80%, 0% 20%, 10% 40%, 20% 0%, 30% 30%, 22% 50%,
+      50% 50%, 65% 20%, 90% 30%;
+  }
+
+  100% {
+    background-position: 0% 70%, 0% 10%, 10% 30%, 20% -10%, 30% 20%, 22% 40%,
+      50% 40%, 65% 10%, 90% 20%;
+    background-size: 0% 0%, 0% 0%, 0% 0%, 0% 0%, 0% 0%, 0% 0%;
+  }
+`;
+const pangpangbot = keyframes`
+0% {
+    background-position: 10% -10%, 30% 10%, 55% -10%, 70% -10%, 85% -10%,
+      70% -10%, 70% 0%;
+  }
+
+  50% {
+    background-position: 0% 80%, 20% 80%, 45% 60%, 60% 100%, 75% 70%, 95% 60%,
+      105% 0%;
+  }
+
+  100% {
+    background-position: 0% 90%, 20% 90%, 45% 70%, 60% 110%, 75% 80%, 95% 70%,
+      110% 10%;
+    background-size: 0% 0%, 0% 0%, 0% 0%, 0% 0%, 0% 0%, 0% 0%;
+  }`;
 export const LikeWrap = styled.div`
   display: flex;
   align-items: center;
@@ -120,14 +154,51 @@ export const LikeWrap = styled.div`
   & > p {
   }
   & > button {
-    /* width: 2rem;
-      height: 2rem; */
     cursor: pointer;
     color: #ffc41d;
     background-color: transparent;
     border: 0px;
+    position: relative;
     &:hover {
-      filter: brightness(50%);
+      /* filter: brightness(50%); */
+    }
+
+    transition: all 0.2s ease;
+
+    &::before,
+    &::after {
+      position: absolute;
+      content: "";
+      width: 150%;
+      left: 50%;
+      top: 50%;
+      height: 150%;
+      transform: translate(-50%, -30%);
+      z-index: -1000;
+      background-repeat: no-repeat;
+    }
+    &:active::before {
+      top: -50%;
+      background-image: radial-gradient(circle, #ffc41d 20%, transparent 20%),
+        radial-gradient(circle, transparent 20%, #ffc41d 20%, transparent 30%),
+        radial-gradient(circle, #ffc41d 20%, transparent 20%), radial-gradient(circle, #ffc41d 20%, transparent 20%),
+        radial-gradient(circle, transparent 10%, #ffc41d 15%, transparent 20%),
+        radial-gradient(circle, #ffc41d 20%, transparent 20%), radial-gradient(circle, #ffc41d 20%, transparent 20%),
+        radial-gradient(circle, #ffc41d 20%, transparent 20%), radial-gradient(circle, #ffc41d 20%, transparent 20%);
+      background-size: 10% 10%, 20% 20%, 15% 15%, 20% 20%, 18% 18%, 10% 10%, 15% 15%, 10% 10%, 18% 18%;
+      background-position: 20% 100%;
+      animation: ${pangpang} 0.4s ease;
+    }
+    &:active::after {
+      bottom: -50%;
+      background-image: radial-gradient(circle, #ffc41d 20%, transparent 20%),
+        radial-gradient(circle, #ffc41d 20%, transparent 20%),
+        radial-gradient(circle, transparent 10%, #ffc41d 15%, transparent 20%),
+        radial-gradient(circle, #ffc41d 20%, transparent 20%), radial-gradient(circle, #ffc41d 20%, transparent 20%),
+        radial-gradient(circle, #ffc41d 20%, transparent 20%), radial-gradient(circle, #ffc41d 20%, transparent 20%);
+      background-size: 15% 15%, 20% 20%, 18% 18%, 20% 20%, 15% 15%, 20% 20%, 18% 18%;
+      background-position: 30% 0%;
+      animation: ${pangpangbot} 0.3s ease;
     }
   }
 `;
